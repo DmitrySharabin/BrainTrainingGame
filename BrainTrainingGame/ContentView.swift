24bf2ec9cecdbe9c44bfafb8d33a74ec64de18f1
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-10-28 15:44:33 +0300

    Add some new logic. Improve styling

--- BrainTrainingGame/ContentView.swift
+++ BrainTrainingGame/ContentView.swift
@@ -12,18 +12,16 @@ struct MoveEmoji: View {
     
     var body: some View {
         Text(emoji)
-            .font(.system(size: 100))
+            .font(.system(size: 80))
     }
 }
 
 struct EmojiButton: ViewModifier {
     func body(content: Content) -> some View {
-        ZStack {
-            Color.mint
-                .clipShape(Circle())
-            
-            content
-        }
+        content
+            .padding(10)
+            .background(.ultraThinMaterial)
+            .clipShape(Circle())
     }
 }
 
@@ -43,44 +41,91 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     
+    @State private var correctMoveCount = 0
+    @State private var wrongMoveCount = 0
+    
     @State private var roundsLeft = 10
     @State private var showingRestartGame = false
     
+    var message: String {
+        "\(roundsLeft) round\(roundsLeft > 1 ? "s" : "") left"
+    }
+    
     var body: some View {
-        VStack {
-            Text("Rock, Paper, Scissors")
-                .font(.largeTitle.bold())
+        ZStack {
+            RadialGradient(gradient: Gradient(stops: [
+                .init(color: Color(red: 0.2, green: 0.2, blue: 0.2), location: 0.8),
+                .init(color: Color(red: 0.75, green: 0.75, blue: 0.75), location: 0.8)
+            ]), center: .bottom, startRadius: 200, endRadius: 670)
+                .ignoresSafeArea()
             
             VStack {
-                MoveEmoji(emoji: moves[currentChoice])
+                Spacer()
                 
-                Text("You should")
-                Text(shouldWin ? "Win" : "Lose")
-            }
-            
-            VStack {
-                Text("Make your move")
+                Group {
+                    Text("Rock, Paper, Scissors")
+                        .font(.largeTitle.weight(.heavy))
+                        .foregroundStyle(.primary)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        MoveEmoji(emoji: moves[currentChoice])
+                        
+                        Text("You should")
+                        Text(shouldWin ? "Win" : "Lose")
+                            .textCase(.uppercase)
+                            .font(.title.weight(.semibold))
+                    }
+                }
                 
-                HStack {
-                    ForEach(0..<3) { number in
-                        Button {
-                            moveTapped(number)
-                        } label: {
-                            MoveEmoji(emoji: moves[number])
-                                .emojiButton()
+                Spacer()
+                Spacer()
+                
+                VStack {
+                    Text("Make your move")
+                    
+                    HStack(spacing: 20) {
+                        ForEach(0..<3) { number in
+                            Button {
+                                moveTapped(number)
+                            } label: {
+                                MoveEmoji(emoji: moves[number])
+                                    .emojiButton()
+                            }
                         }
                     }
                 }
+                
+                Spacer()
+                Spacer()
+                
+                HStack(spacing: 50) {
+                    VStack {
+                        Text("Correct")
+                            .textCase(.uppercase)
+                            
+                        Text("\(correctMoveCount)")
+                            .font(.title.bold())
+                    }
+                    
+                    VStack {
+                        Text("Wrong")
+                            .textCase(.uppercase)
+                        
+                        Text("\(wrongMoveCount)")
+                            .font(.title.bold())
+                    }
+                    
+                }
             }
-            
-            Text("Score: \(score)")
-                .font(.title.bold())
+            .foregroundColor(.white)
+            .padding()
         }
-        .padding()
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("New round", action: newRound)
         } message: {
-            Text("Your score is \(score).")
+            Text(message)
         }
         .alert(scoreTitle, isPresented: $showingRestartGame) {
             Button("Restart game", action: reset)
@@ -103,9 +148,11 @@ struct ContentView: View {
         
         if moves[move] == correctMove {
             scoreTitle = "Correct"
+            correctMoveCount += 1
             score += 1
         } else {
-            scoreTitle = "Wrong! You should choose \(correctMove)"
+            scoreTitle = "You should choose \(correctMove)"
+            wrongMoveCount += 1
             if score > 0 {
                 score -= 1
             }
@@ -126,6 +173,8 @@ struct ContentView: View {
     
     func reset() {
         roundsLeft = 10
+        correctMoveCount = 0
+        wrongMoveCount = 0
         score = 0
         newRound()
     }
