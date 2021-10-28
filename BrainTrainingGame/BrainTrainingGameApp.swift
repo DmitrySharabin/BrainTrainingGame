48512d53bbb1628cff65c7a89e9646ac30ef6891
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-10-28 11:24:07 +0300

    Initial Commit

---
+++ BrainTrainingGame/BrainTrainingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainTrainingGameApp.swift
+//  BrainTrainingGame
+//
+//  Created by Dmitry Sharabin on 28.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct BrainTrainingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrainTrainingGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BrainTrainingGame
+//
+//  Created by Dmitry Sharabin on 28.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
